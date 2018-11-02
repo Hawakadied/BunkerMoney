48978d44911a2f4333fc4f0cd0b84e0da25ab3f7
Complexicon <complexiconyt@gmail.com>
2018-11-03 00:36:10 +0100

    Projektdateien hinzufügen.

---
+++ BunkerMoney/Main.Designer.cs
@@ -0,0 +1,186 @@
+﻿namespace BunkerMoney
+{
+	partial class Main
+	{
+		/// <summary>
+		/// Erforderliche Designervariable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Verwendete Ressourcen bereinigen.
+		/// </summary>
+		/// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Vom Windows Form-Designer generierter Code
+
+		/// <summary>
+		/// Erforderliche Methode für die Designerunterstützung.
+		/// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.button1 = new System.Windows.Forms.Button();
+			this.label1 = new System.Windows.Forms.Label();
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.label2 = new System.Windows.Forms.Label();
+			this.textBox2 = new System.Windows.Forms.TextBox();
+			this.label3 = new System.Windows.Forms.Label();
+			this.textBox3 = new System.Windows.Forms.TextBox();
+			this.progressBar1 = new System.Windows.Forms.ProgressBar();
+			this.button2 = new System.Windows.Forms.Button();
+			this.label4 = new System.Windows.Forms.Label();
+			this.checkBox1 = new System.Windows.Forms.CheckBox();
+			this.SuspendLayout();
+			// 
+			// button1
+			// 
+			this.button1.Location = new System.Drawing.Point(12, 136);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(89, 31);
+			this.button1.TabIndex = 6;
+			this.button1.Text = "Apply";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.ApplyButton);
+			// 
+			// label1
+			// 
+			this.label1.Cursor = System.Windows.Forms.Cursors.Default;
+			this.label1.Location = new System.Drawing.Point(12, 9);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(210, 13);
+			this.label1.TabIndex = 1;
+			this.label1.Text = "Amount of Delivery Packages";
+			this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+			// 
+			// textBox1
+			// 
+			this.textBox1.Location = new System.Drawing.Point(12, 25);
+			this.textBox1.Name = "textBox1";
+			this.textBox1.Size = new System.Drawing.Size(210, 20);
+			this.textBox1.TabIndex = 0;
+			// 
+			// label2
+			// 
+			this.label2.Location = new System.Drawing.Point(12, 48);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(210, 15);
+			this.label2.TabIndex = 3;
+			this.label2.Text = "Sell Price for all Packages";
+			this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+			// 
+			// textBox2
+			// 
+			this.textBox2.Location = new System.Drawing.Point(12, 66);
+			this.textBox2.Name = "textBox2";
+			this.textBox2.Size = new System.Drawing.Size(210, 20);
+			this.textBox2.TabIndex = 2;
+			// 
+			// label3
+			// 
+			this.label3.Location = new System.Drawing.Point(11, 89);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(211, 18);
+			this.label3.TabIndex = 5;
+			this.label3.Text = "Money you shall Receive (in Million)";
+			this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+			// 
+			// textBox3
+			// 
+			this.textBox3.Location = new System.Drawing.Point(12, 110);
+			this.textBox3.Name = "textBox3";
+			this.textBox3.Size = new System.Drawing.Size(210, 20);
+			this.textBox3.TabIndex = 4;
+			// 
+			// progressBar1
+			// 
+			this.progressBar1.Location = new System.Drawing.Point(12, 173);
+			this.progressBar1.Name = "progressBar1";
+			this.progressBar1.Size = new System.Drawing.Size(210, 14);
+			this.progressBar1.TabIndex = 7;
+			// 
+			// button2
+			// 
+			this.button2.Location = new System.Drawing.Point(131, 136);
+			this.button2.Name = "button2";
+			this.button2.Size = new System.Drawing.Size(91, 31);
+			this.button2.TabIndex = 8;
+			this.button2.Text = "Exit";
+			this.button2.UseVisualStyleBackColor = true;
+			this.button2.Click += new System.EventHandler(this.ExitButton);
+			// 
+			// label4
+			// 
+			this.label4.AutoSize = true;
+			this.label4.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+			this.label4.Location = new System.Drawing.Point(12, 194);
+			this.label4.Name = "label4";
+			this.label4.Size = new System.Drawing.Size(167, 13);
+			this.label4.TabIndex = 9;
+			this.label4.Text = "Complexicon\'s BunkerMoney v0.1";
+			// 
+			// checkBox1
+			// 
+			this.checkBox1.AutoSize = true;
+			this.checkBox1.Location = new System.Drawing.Point(182, 193);
+			this.checkBox1.Name = "checkBox1";
+			this.checkBox1.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+			this.checkBox1.Size = new System.Drawing.Size(40, 17);
+			this.checkBox1.TabIndex = 10;
+			this.checkBox1.Text = "SC";
+			this.checkBox1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+			this.checkBox1.UseVisualStyleBackColor = false;
+			this.checkBox1.Click += new System.EventHandler(this.SCCheck);
+			// 
+			// Main
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(234, 216);
+			this.Controls.Add(this.checkBox1);
+			this.Controls.Add(this.label4);
+			this.Controls.Add(this.button2);
+			this.Controls.Add(this.progressBar1);
+			this.Controls.Add(this.textBox3);
+			this.Controls.Add(this.label3);
+			this.Controls.Add(this.textBox2);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.textBox1);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.button1);
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+			this.MaximizeBox = false;
+			this.MinimizeBox = false;
+			this.Name = "Main";
+			this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Show;
+			this.Text = "BunkerMoney";
+			this.Load += new System.EventHandler(this.OnLoad);
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.TextBox textBox1;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.TextBox textBox2;
+		private System.Windows.Forms.Label label3;
+		private System.Windows.Forms.TextBox textBox3;
+		private System.Windows.Forms.ProgressBar progressBar1;
+		private System.Windows.Forms.Button button2;
+		private System.Windows.Forms.Label label4;
+		private System.Windows.Forms.CheckBox checkBox1;
+	}
+}
+
---
+++ BunkerMoney/Main.cs
@@ -0,0 +1,187 @@
+﻿using System;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+
+namespace BunkerMoney
+{
+	public partial class Main : Form
+	{
+
+		public Process proc;
+		public long BaseAddress;
+
+		[DllImport("kernel32.dll")]
+		public static extern int WriteProcessMemory(IntPtr Handle, long Address, byte[] buffer, int Size, int BytesWritten = 0);
+		[DllImport("kernel32.dll")]
+		public static extern int ReadProcessMemory(IntPtr Handle, long Address, byte[] buffer, int Size, int BytesRead = 0);
+
+		public Main()
+		{
+			InitializeComponent();
+		}
+
+		private void ApplyButton(object sender, EventArgs e)
+		{
+			if (IsNum(textBox1.Text) && IsNum(textBox2.Text) && IsNum(textBox3.Text))
+			{
+				try
+				{
+					int amt = ParseNum(textBox1.Text);
+					int price = ParseNum(textBox2.Text);
+					int multiplier = ParseNum(textBox3.Text);
+
+					proc = getProcess("GTA5");
+					progressBar1.Value = 10;
+					BaseAddress = GetBaseAddress("GTA5.exe");
+					progressBar1.Value = 25;
+					long ptr = GetPointerAddress(BaseAddress + 0x2C9EB78, new int[] { 0x1180, 0x4088 });
+					progressBar1.Value = 30;
+
+					int pricePerPacket = price / amt;
+					int amtForMil = 0;
+					for (int i = 1; (i * pricePerPacket) < 1000000; i++)
+					{
+						amtForMil = i;
+					}
+					progressBar1.Value = 80;
+
+					WriteInteger(ptr, (amtForMil * multiplier));
+					progressBar1.Value = 100;
+					MessageBox.Show("Done! Destroy Delivery Vehicle and Enjoy :)", "Yay!");
+					progressBar1.Value = 0;
+				}
+				catch
+				{
+					progressBar1.Value = 100;
+					MessageBox.Show("There was an Error during the Procedure!", "Error");
+					progressBar1.Value = 0;
+				}
+			}
+			else
+			{
+				progressBar1.Value = 100;
+				MessageBox.Show("Please make sure you used Numbers!", "Warning");
+				progressBar1.Value = 0;
+			}
+		}
+
+		private void SCCheck(object sender, EventArgs e)
+		{
+			MessageBox.Show("This Box does not do anything right now \nIn new versions it will be used to Toggle SocialClub Mode", "Warning");
+		}
+
+		private void ExitButton(object sender, EventArgs e)
+		{
+			Application.Exit();
+		}
+
+		private void OnLoad(object sender, EventArgs e)
+		{
+			//IsGameRunning();
+		}
+
+		private int ParseNum(string s)
+		{
+			int i;
+			if (!int.TryParse(s, out i))
+			{
+				return -13371337;
+			}
+			return i;
+		}
+
+		private bool IsNum(string s)
+		{
+			int x = ParseNum(s);
+			if (x == -13371337)
+			{
+				MessageBox.Show(s + " is not a Number!", "Warning");
+				return false;
+			}
+			return true;
+		}
+
+		public static bool IsGameRunning()
+		{
+			Process[] process = Process.GetProcessesByName("GTA5");
+			if (process.Length == 0)
+			{
+				if (MessageBox.Show("You need to run GTA5. Closing Menu!", "Critical Scary Error") == System.Windows.Forms.DialogResult.OK)
+				{
+
+				}
+				Application.Exit();
+				return false;
+			}
+			return true;
+		}
+
+		public IntPtr GetProcessHandle()
+		{
+			try
+			{
+				return proc.Handle;
+			}
+			catch
+			{
+				IsGameRunning();
+				return IntPtr.Zero;
+			}
+		}
+
+		public Process getProcess(string name)
+		{
+			return Process.GetProcessesByName(name)[0];
+		}
+
+		public void WriteInteger(long Address, int Value)
+		{
+			WriteProcessMemory(GetProcessHandle(), Address, BitConverter.GetBytes(Value), 4);
+		}
+
+		public long GetBaseAddress(string ModuleName)
+		{
+			try
+			{
+				ProcessModuleCollection modules = proc.Modules;
+				ProcessModule DLLBaseAddress = null;
+
+				foreach (ProcessModule i in modules)
+				{
+					if (i.ModuleName == ModuleName)
+					{
+						DLLBaseAddress = i;
+					}
+				}
+
+				return DLLBaseAddress.BaseAddress.ToInt64();
+			}
+			catch
+			{
+				return 0;
+			}
+		}
+
+		public long GetPointerAddress(long Pointer, int[] Offset = null)
+		{
+			byte[] Buffer = new byte[8];
+
+			ReadProcessMemory(GetProcessHandle(), Pointer, Buffer, Buffer.Length);
+
+			if (Offset != null)
+			{
+				for (int x = 0; x < (Offset.Length - 1); x++)
+				{
+					Pointer = BitConverter.ToInt64(Buffer, 0) + Offset[x];
+					ReadProcessMemory(GetProcessHandle(), Pointer, Buffer, Buffer.Length);
+				}
+
+				Pointer = BitConverter.ToInt64(Buffer, 0) + Offset[Offset.Length - 1];
+			}
+
+			return Pointer;
+		}
+
+	}
+}
