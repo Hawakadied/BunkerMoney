f9eb117ddbe3994cbea12022bcb5b572719e0648
Philipp Leer <philippleer@gmail.com>
2019-02-11 19:34:06 +0100

    Cosmetic Patches; Updated Message after GTA Update requires to deliver the stuff.

--- BunkerMoney/Main.Designer.cs
+++ BunkerMoney/Main.Designer.cs
@@ -31,112 +31,141 @@
 		{
 			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Main));
 			this.button1 = new System.Windows.Forms.Button();
-			this.label1 = new System.Windows.Forms.Label();
-			this.priceBox = new System.Windows.Forms.TextBox();
+			this.delivery = new System.Windows.Forms.TextBox();
 			this.label2 = new System.Windows.Forms.Label();
-			this.deliverAmtBox = new System.Windows.Forms.TextBox();
-			this.label3 = new System.Windows.Forms.Label();
-			this.wantedMoneyBox = new System.Windows.Forms.TextBox();
 			this.info = new System.Windows.Forms.Label();
-			this.scBox = new System.Windows.Forms.CheckBox();
+			this.panel1 = new System.Windows.Forms.Panel();
+			this.label1 = new System.Windows.Forms.Label();
+			this.panel2 = new System.Windows.Forms.Panel();
+			this.initPrice = new System.Windows.Forms.TextBox();
+			this.label3 = new System.Windows.Forms.Label();
+			this.panel3 = new System.Windows.Forms.Panel();
+			this.money = new System.Windows.Forms.TextBox();
+			this.panel1.SuspendLayout();
+			this.panel2.SuspendLayout();
+			this.panel3.SuspendLayout();
 			this.SuspendLayout();
 			// 
 			// button1
 			// 
-			this.button1.Location = new System.Drawing.Point(12, 136);
+			this.button1.Location = new System.Drawing.Point(8, 130);
 			this.button1.Name = "button1";
-			this.button1.Size = new System.Drawing.Size(84, 31);
+			this.button1.Size = new System.Drawing.Size(119, 31);
 			this.button1.TabIndex = 6;
 			this.button1.Text = "Apply";
 			this.button1.UseVisualStyleBackColor = true;
 			this.button1.Click += new System.EventHandler(this.ApplyButton);
 			// 
-			// label1
+			// delivery
 			// 
-			this.label1.Cursor = System.Windows.Forms.Cursors.Default;
-			this.label1.Location = new System.Drawing.Point(12, 50);
-			this.label1.Name = "label1";
-			this.label1.Size = new System.Drawing.Size(130, 13);
-			this.label1.TabIndex = 1;
-			this.label1.Text = "Delivery Packages";
-			this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
-			// 
-			// priceBox
-			// 
-			this.priceBox.Cursor = System.Windows.Forms.Cursors.IBeam;
-			this.priceBox.Location = new System.Drawing.Point(12, 25);
-			this.priceBox.Name = "priceBox";
-			this.priceBox.Size = new System.Drawing.Size(130, 20);
-			this.priceBox.TabIndex = 0;
+			this.delivery.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+			this.delivery.Cursor = System.Windows.Forms.Cursors.IBeam;
+			this.delivery.Location = new System.Drawing.Point(3, 3);
+			this.delivery.Name = "delivery";
+			this.delivery.Size = new System.Drawing.Size(111, 20);
+			this.delivery.TabIndex = 0;
 			// 
 			// label2
 			// 
-			this.label2.Location = new System.Drawing.Point(12, 7);
+			this.label2.AutoSize = true;
+			this.label2.Location = new System.Drawing.Point(12, 48);
 			this.label2.Name = "label2";
-			this.label2.Size = new System.Drawing.Size(130, 15);
+			this.label2.Size = new System.Drawing.Size(96, 13);
 			this.label2.TabIndex = 3;
-			this.label2.Text = "Initial Sell Price";
-			this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
-			// 
-			// deliverAmtBox
-			// 
-			this.deliverAmtBox.Location = new System.Drawing.Point(12, 66);
-			this.deliverAmtBox.Name = "deliverAmtBox";
-			this.deliverAmtBox.Size = new System.Drawing.Size(130, 20);
-			this.deliverAmtBox.TabIndex = 2;
-			// 
-			// label3
-			// 
-			this.label3.Location = new System.Drawing.Point(12, 94);
-			this.label3.Name = "label3";
-			this.label3.Size = new System.Drawing.Size(131, 13);
-			this.label3.TabIndex = 5;
-			this.label3.Text = "Money (in Million)";
-			this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
-			// 
-			// wantedMoneyBox
-			// 
-			this.wantedMoneyBox.Location = new System.Drawing.Point(12, 110);
-			this.wantedMoneyBox.Name = "wantedMoneyBox";
-			this.wantedMoneyBox.Size = new System.Drawing.Size(130, 20);
-			this.wantedMoneyBox.TabIndex = 4;
+			this.label2.Text = "Delivery Packages";
+			this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
 			// 
 			// info
 			// 
 			this.info.AutoSize = true;
 			this.info.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
-			this.info.Location = new System.Drawing.Point(9, 170);
+			this.info.Location = new System.Drawing.Point(5, 164);
 			this.info.Name = "info";
-			this.info.Size = new System.Drawing.Size(114, 13);
+			this.info.Size = new System.Drawing.Size(118, 13);
 			this.info.TabIndex = 9;
-			this.info.Text = "v1337 by Complexicon";
+			this.info.Text = "by Complexicon with <3";
+			this.info.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+			this.info.Click += new System.EventHandler(this.info_Click);
 			// 
-			// scBox
+			// panel1
 			// 
-			this.scBox.AutoSize = true;
-			this.scBox.Location = new System.Drawing.Point(102, 144);
-			this.scBox.Name = "scBox";
-			this.scBox.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
-			this.scBox.Size = new System.Drawing.Size(40, 17);
-			this.scBox.TabIndex = 10;
-			this.scBox.Text = "SC";
-			this.scBox.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
-			this.scBox.UseVisualStyleBackColor = false;
-			this.scBox.Click += new System.EventHandler(this.SCCheck);
+			this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+			this.panel1.Controls.Add(this.delivery);
+			this.panel1.Location = new System.Drawing.Point(8, 57);
+			this.panel1.Margin = new System.Windows.Forms.Padding(2, 4, 2, 2);
+			this.panel1.Name = "panel1";
+			this.panel1.Size = new System.Drawing.Size(119, 28);
+			this.panel1.TabIndex = 10;
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(12, 9);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(78, 13);
+			this.label1.TabIndex = 11;
+			this.label1.Text = "Initial Sell Price";
+			this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+			// 
+			// panel2
+			// 
+			this.panel2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+			this.panel2.Controls.Add(this.initPrice);
+			this.panel2.Location = new System.Drawing.Point(8, 18);
+			this.panel2.Margin = new System.Windows.Forms.Padding(2, 4, 2, 2);
+			this.panel2.Name = "panel2";
+			this.panel2.Size = new System.Drawing.Size(119, 28);
+			this.panel2.TabIndex = 12;
+			// 
+			// initPrice
+			// 
+			this.initPrice.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+			this.initPrice.Cursor = System.Windows.Forms.Cursors.IBeam;
+			this.initPrice.Location = new System.Drawing.Point(3, 3);
+			this.initPrice.Name = "initPrice";
+			this.initPrice.Size = new System.Drawing.Size(111, 20);
+			this.initPrice.TabIndex = 0;
+			// 
+			// label3
+			// 
+			this.label3.AutoSize = true;
+			this.label3.Location = new System.Drawing.Point(12, 87);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(88, 13);
+			this.label3.TabIndex = 13;
+			this.label3.Text = "Money (in Million)";
+			this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+			// 
+			// panel3
+			// 
+			this.panel3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+			this.panel3.Controls.Add(this.money);
+			this.panel3.Location = new System.Drawing.Point(8, 96);
+			this.panel3.Name = "panel3";
+			this.panel3.Size = new System.Drawing.Size(119, 28);
+			this.panel3.TabIndex = 14;
+			// 
+			// money
+			// 
+			this.money.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+			this.money.Cursor = System.Windows.Forms.Cursors.IBeam;
+			this.money.Location = new System.Drawing.Point(3, 3);
+			this.money.Name = "money";
+			this.money.Size = new System.Drawing.Size(111, 20);
+			this.money.TabIndex = 0;
 			// 
 			// Main
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(154, 186);
-			this.Controls.Add(this.scBox);
-			this.Controls.Add(this.info);
-			this.Controls.Add(this.wantedMoneyBox);
+			this.ClientSize = new System.Drawing.Size(134, 176);
 			this.Controls.Add(this.label3);
-			this.Controls.Add(this.deliverAmtBox);
-			this.Controls.Add(this.label2);
-			this.Controls.Add(this.priceBox);
+			this.Controls.Add(this.panel3);
 			this.Controls.Add(this.label1);
+			this.Controls.Add(this.panel2);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.panel1);
+			this.Controls.Add(this.info);
 			this.Controls.Add(this.button1);
 			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
 			this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
@@ -144,7 +173,12 @@
 			this.MinimizeBox = false;
 			this.Name = "Main";
 			this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Show;
-			this.Text = "BunkerMoney";
+			this.panel1.ResumeLayout(false);
+			this.panel1.PerformLayout();
+			this.panel2.ResumeLayout(false);
+			this.panel2.PerformLayout();
+			this.panel3.ResumeLayout(false);
+			this.panel3.PerformLayout();
 			this.ResumeLayout(false);
 			this.PerformLayout();
 
@@ -153,14 +187,16 @@
 		#endregion
 
 		private System.Windows.Forms.Button button1;
-		private System.Windows.Forms.Label label1;
-		private System.Windows.Forms.TextBox priceBox;
+		private System.Windows.Forms.TextBox delivery;
 		private System.Windows.Forms.Label label2;
-		private System.Windows.Forms.TextBox deliverAmtBox;
-		private System.Windows.Forms.Label label3;
-		private System.Windows.Forms.TextBox wantedMoneyBox;
 		private System.Windows.Forms.Label info;
-		private System.Windows.Forms.CheckBox scBox;
+		private System.Windows.Forms.Panel panel1;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Panel panel2;
+		private System.Windows.Forms.TextBox initPrice;
+		private System.Windows.Forms.Label label3;
+		private System.Windows.Forms.Panel panel3;
+		private System.Windows.Forms.TextBox money;
 	}
 }
 
--- BunkerMoney/Main.cs
+++ BunkerMoney/Main.cs
@@ -4,10 +4,8 @@ using System.Net;
 using System.Runtime.InteropServices;
 using System.Windows.Forms;
 
-namespace BunkerMoney
-{
-	public partial class Main : Form
-	{
+namespace BunkerMoney {
+	public partial class Main : Form {
 
 		[DllImport("kernel32.dll")]
 		public static extern int WriteProcessMemory(IntPtr Handle, long Address, byte[] buffer, int Size, int BytesWritten = 0);
@@ -23,105 +21,79 @@ namespace BunkerMoney
 
 		int[] bunkerOff = new int[] { 0x1180, 0x4128 };
 
-		bool isSC = false;
-
 		string url = "https://raw.githubusercontent.com/Complexicon/BunkerMoney/master/BunkerMoney/offsets.ini";
 
-		public Main()
-		{
+		public Main() {
 
-			using (WebClient client = new WebClient())
-			{
-				try
-				{
+			using (WebClient client = new WebClient()) {
+				try {
 					string s = client.DownloadString(url);
 					string[] lines = s.Split(Environment.NewLine.ToCharArray());
-					foreach (string l in lines)
-					{
+					foreach (string l in lines) {
 						if (l.StartsWith("scPTR")) scPTR = Convert.ToInt32(l.Substring(6), 16);
 						if (l.StartsWith("steamPTR")) steamPTR = Convert.ToInt32(l.Substring(9), 16);
 						if (l.StartsWith("offset1")) bunkerOff[0] = Convert.ToInt32(l.Substring(8), 16);
 						if (l.StartsWith("offset2")) bunkerOff[1] = Convert.ToInt32(l.Substring(8), 16);
 					}
-				}
-				catch
-				{
-					MessageBox.Show("Couldn't load Remote Offsets! Using local Offsets!", "Warning");
+				} catch {
+					MessageBox.Show("Couldn't load Remote Offsets! Using local Offsets (GTA 1.46)!", "Warning");
 				}
 			}
 
 
 			InitializeComponent();
-			version = "v0.3";
-			info.Text = version + " by Complexicon";
+			this.Text = "v0.4.1";
 		}
 
-		private void ApplyButton(object sender, EventArgs e)
-		{
-			if (CheckGTA())
-			{
-				if (IsNum(priceBox.Text) && IsNum(deliverAmtBox.Text) && IsNum(wantedMoneyBox.Text))
-				{
-					try
-					{
-						int price = ParseNum(priceBox.Text);
-						int amt = ParseNum(deliverAmtBox.Text);
-						int multiplier = ParseNum(wantedMoneyBox.Text);
+		private void ApplyButton(object sender, EventArgs e) {
+			if (CheckGTA()) {
+				if (IsNum(initPrice.Text) && IsNum(delivery.Text) && IsNum(money.Text)) {
+					try {
+						int price = ParseNum(initPrice.Text);
+						int amt = ParseNum(delivery.Text);
+						int multiplier = ParseNum(money.Text);
 
 						int amtForMil = 1000000 / (price / amt);
 
 						proc = GetProcess("GTA5");
 						baseAddr = GetBaseAddress("GTA5.exe");
 
-						long ptr = isSC
+						long ptr = MessageBox.Show("Social Club?", "This some Important Shit", MessageBoxButtons.YesNo) == DialogResult.Yes
 							? GetPtrAddr(baseAddr + scPTR, bunkerOff)
 							: GetPtrAddr(baseAddr + steamPTR, bunkerOff);
 
 						WriteInteger(ptr, (amtForMil * multiplier));
-						MessageBox.Show("Done! Check the Delivery Amount and if correct Destroy Delivery Vehicle and Enjoy :)", "Yay!");
+						MessageBox.Show("Done! Deliver Vehlicle and Enjoy :D, Program will now close!", "Yay!");
+						Environment.Exit(0);
 
 					} catch { MessageBox.Show("There was an Error during the Procedure!", "Error"); }
 				} else { MessageBox.Show("Please make sure you used Numbers!", "Warning"); }
 			}
 		}
 
-		private void SCCheck(object sender, EventArgs e)
-		{
-			MessageBox.Show((scBox.Checked ? "Activated" : "Deactivated") + " Socialclub Mode", "Info");
-			isSC = scBox.Checked;
-		}
-
 		private static int ParseNum(string s) => !int.TryParse(s, out int i) ? -13371337 : i;
 
-		private static bool IsNum(string s)
-		{
+		private static bool IsNum(string s) {
 			int x = ParseNum(s);
-			if (x == -13371337)
-			{
+			if (x == -13371337) {
 				MessageBox.Show("'" + s + "' is not a Number!", "Warning");
 				return false;
 			}
 			return true;
 		}
 
-		public static bool CheckGTA()
-		{
-			if (Process.GetProcessesByName("GTA5").Length == 0)
-			{
+		public static bool CheckGTA() {
+			if (Process.GetProcessesByName("GTA5").Length == 0) {
 				MessageBox.Show("Please Make Sure GTA5 is Running!", "Critical Error");
 				return false;
 			}
 			return true;
 		}
 
-		public IntPtr GetProcessHandle()
-		{
-			try
-			{
+		public IntPtr GetProcessHandle() {
+			try {
 				return proc.Handle;
-			}
-			catch
-			{
+			} catch {
 				CheckGTA();
 				return IntPtr.Zero;
 			}
@@ -131,37 +103,28 @@ namespace BunkerMoney
 
 		public void WriteInteger(long Address, int Value) => WriteProcessMemory(GetProcessHandle(), Address, BitConverter.GetBytes(Value), 4);
 
-		public long GetBaseAddress(string ModuleName)
-		{
-			try
-			{
+		public long GetBaseAddress(string ModuleName) {
+			try {
 				ProcessModule DLLBaseAddress = null;
-				foreach (ProcessModule i in proc.Modules)
-				{
-					if (i.ModuleName == ModuleName)
-					{
+				foreach (ProcessModule i in proc.Modules) {
+					if (i.ModuleName == ModuleName) {
 						DLLBaseAddress = i;
 					}
 				}
 
 				return DLLBaseAddress.BaseAddress.ToInt64();
-			}
-			catch
-			{
+			} catch {
 				return 0;
 			}
 		}
 
-		public long GetPtrAddr(long Pointer, int[] Offset = null)
-		{
+		public long GetPtrAddr(long Pointer, int[] Offset = null) {
 			byte[] Buffer = new byte[8];
 
 			ReadProcessMemory(GetProcessHandle(), Pointer, Buffer, Buffer.Length);
 
-			if (Offset != null)
-			{
-				for (int x = 0; x < (Offset.Length - 1); x++)
-				{
+			if (Offset != null) {
+				for (int x = 0; x < (Offset.Length - 1); x++) {
 					Pointer = BitConverter.ToInt64(Buffer, 0) + Offset[x];
 					ReadProcessMemory(GetProcessHandle(), Pointer, Buffer, Buffer.Length);
 				}
@@ -172,5 +135,6 @@ namespace BunkerMoney
 			return Pointer;
 		}
 
+		private void info_Click(object sender, EventArgs e) => MessageBox.Show("Easter Egg Hunter ;)");
 	}
 }
